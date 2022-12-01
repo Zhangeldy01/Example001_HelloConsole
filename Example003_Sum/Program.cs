12136d886d6f60795d9b92b3d0fc1e7ceb4b037c
Zhangeldy <shakhanov_zhan@mail.ru>
2022-12-01 16:35:48 +0300

    add Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
 ﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
 int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + NumberB;
 Console.WriteLine(result);
